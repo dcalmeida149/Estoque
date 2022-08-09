1cf593721693813ae6622587acb106fb88821f6d
dcalmeida149 <danielalmeida.dca@outlook.com>
2022-08-09 19:45:25 -0300

    conteudo aula 68

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,10 +1,30 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Product;
+
 public class Main {
 
 	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
 		
+		Product product = new Product();
+		
+		System.out.println("Entre com os dados do produto");
+		System.out.print("Nome: ");
+		product.name = sc.nextLine();
+		
+		System.out.print("Quantidade: ");
+		product.quantity = sc.nextInt();
 
+		System.out.print("Preco: ");
+		product.Price = sc.nextDouble();
+		 
+		
+		sc.close();
 	}
 
 }
