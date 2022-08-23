bb6d908bafbba8cd6fb2ad6214b66e96407e0f6c
Daniel Almeida <danielalmeida.dca@outlook.com>
2022-08-23 07:18:25 -0300

    aulas 79,80 e 81 Curso Java

--- src/entities/Product.java
+++ src/entities/Product.java
@@ -5,25 +5,37 @@ public class Product {
 	private double Price;
 	private int quantity;
 	
-	public Product(String name, double Price, int quantity) {
+	public Product() {}
+	public Product(String name, double price, int quantity) {
 		this.name = name;
-		this.Price = Price;
+		Price = price;
 		this.quantity = quantity;
 	}
-	public String getName() { // aula 79 - - encapsulamento
+	public Product(String name, double price) {
+		this.name = name;
+		Price = price;
+	}
+	public String getName() {
 		return name;
 	}
-	public void setName(String name) { //aula 78 - encapsulamento
+	public void setName(String name) {
 		this.name = name;
+	}
+	public double getPrice() {
+		return Price;
+	}
+	public void setPrice(double price) {
+		Price = price;
+	}
+	public int getQuantity() {
+		return quantity;
 	}
 	public double TotalValueInStock() {
 		return Price * quantity;
 	}
-	
 	public void addProducts(int quantity) {
 		this.quantity += quantity;
 	}
-	
 	public void removeProducts(int quantity) {
 		this.quantity -= quantity;
 	}
